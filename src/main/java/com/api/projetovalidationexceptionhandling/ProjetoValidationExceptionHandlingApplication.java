a46c5cbba74bede23b26c7bfb3977c46dfb8722a
Daniel Penelva <d4n.andrade@gmail.com>
2023-12-07 20:23:09 -0300

    Criando projeto Spring Boot 3 para utilizar Validation e Exception Handling

---
+++ src/main/java/com/api/projetovalidationexceptionhandling/ProjetoValidationExceptionHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.api.projetovalidationexceptionhandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoValidationExceptionHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoValidationExceptionHandlingApplication.class, args);
+	}
+
+}
